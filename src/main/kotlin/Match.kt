01ad535c9e4192343ce0ef34a68ae748aac01ef1
Tim Hobbs <timothy.w.hobbs@gmail.com>
2020-07-28 18:42:31 +1200

    Added tie break logic, fixed some logic around victories and advantage, added tie break tests and real world name tests

--- src/main/kotlin/Match.kt
+++ src/main/kotlin/Match.kt
@@ -4,10 +4,16 @@ class Match(private val player1Name: String, private val player2Name: String) {
 
     private val setScores = arrayOf(0, 0)
     private var gameScores = arrayOf(0, 0)
+    private var isTieBreak = false
+
+    private fun gameScoreThreshold(): Int {
+        return if(isTieBreak) 7 else 4
+    }
 
     private fun currentGameWonByPlayer(playerIndex: Int): Boolean {
         val otherPlayerIndex = 1 - playerIndex
-        return gameScores[playerIndex] >= 4 && gameScores[playerIndex] >= gameScores[otherPlayerIndex] + 2
+        // A player wins if their score is greater than 4 (or 7 in a tie break), and they have  lead of 2 or more
+        return gameScores[playerIndex] >= gameScoreThreshold() && gameScores[playerIndex] >= gameScores[otherPlayerIndex] + 2
     }
 
     private fun currentGameWon(): Boolean {
@@ -24,13 +30,23 @@ class Match(private val player1Name: String, private val player2Name: String) {
         gameScores[playerIndex]++
 
         if (currentGameWon()) {
+            val otherPlayerIndex = 1 - playerIndex
+
+            // If the game-winning player was behind, but has equalized the score to 6-6, the next game is a tie-break
+            if (setScores[playerIndex] == 5 && setScores[otherPlayerIndex] == 6) {
+                isTieBreak = true
+            }
+
             setScores[playerIndex]++
             gameScores = arrayOf(0, 0)
         }
     }
 
     private fun gameScoreToPoints(score: Int): Int {
-        return when(score) {
+        return if (isTieBreak) {
+            score
+        }
+        else when(score) {
             0 -> 0
             1 -> 15
             2 -> 30
@@ -40,6 +56,8 @@ class Match(private val player1Name: String, private val player2Name: String) {
     }
 
     fun gameScore(): String? {
+        val advantageThreshold = gameScoreThreshold() - 1
+
         return if (gameScores[0] == gameScores[1]) {
             when (gameScores[0]) {
                 0 -> null
@@ -47,10 +65,10 @@ class Match(private val player1Name: String, private val player2Name: String) {
                 else -> "Deuce"
             }
         }
-        else if (gameScores[0] > gameScores[1] && gameScores[0] > 3) {
+        else if (gameScores[0] - gameScores[1] == 1 && gameScores[0] >= advantageThreshold) {
             "Advantage $player1Name"
         }
-        else if (gameScores[1] > gameScores[0] && gameScores[1] > 3) {
+        else if (gameScores[1] - gameScores[0] == 1 && gameScores[1] >= advantageThreshold) {
             "Advantage $player2Name"
         }
         else {
--- src/test/kotlin/MatchTests.kt
+++ src/test/kotlin/MatchTests.kt
@@ -25,6 +25,28 @@ class MatchTests {
     }
 
     @Test
+    fun realWorldNamesTest() {
+        val match = Match("Djokovic", "Federer")
+        match.pointWonBy("Djokovic")
+        match.pointWonBy("Federer")
+        assertEquals("0-0, 15-15", match.score())
+
+        match.pointWonBy("Djokovic");
+        match.pointWonBy("Djokovic");
+        assertEquals("0-0, 40-15", match.score());
+
+        match.pointWonBy("Federer");
+        match.pointWonBy("Federer");
+        assertEquals("0-0, Deuce", match.score());
+
+        match.pointWonBy("Djokovic");
+        assertEquals("0-0, Advantage Djokovic", match.score());
+
+        match.pointWonBy("Djokovic");
+        assertEquals("1-0", match.score());
+    }
+
+    @Test
     fun veryHighScoreTest() {
         val match = Match("player 1", "player 2")
 
@@ -76,5 +98,80 @@ class MatchTests {
         }
 
         assertEquals("0-5", match.score());
+    }
+
+    fun winGame(match: Match, playerName: String) {
+        for (i in 0 until 4) {
+            match.pointWonBy(playerName);
+        }
+    }
+
+    fun createTieBreakMatch(leadingPlayerName: String): Match {
+        val match = Match("player 1", "player 2")
+
+        for (i in 0 until 5) {
+            assertEquals("$i-$i", match.score());
+
+            winGame(match, "player 1");
+            winGame(match, "player 2");
+        }
+
+        assertEquals("5-5", match.score());
+
+        winGame(match, leadingPlayerName);
+
+        return match
+    }
+
+    @Test
+    // Test Player 1 winning, from a leading position (no tie-break)
+    fun tieBreakEvadeTest() {
+        val match = createTieBreakMatch("player 1")
+        assertEquals("6-5", match.score());
+
+        winGame(match, "player 1");
+
+        assertEquals("7-5", match.score());
+    }
+
+    @Test
+    fun tieBreakTest() {
+
+        val match = createTieBreakMatch("player 1")
+        assertEquals("6-5", match.score());
+
+        winGame(match, "player 2");
+        assertEquals("6-6", match.score());
+
+        // Game score does not undergo the conversion to points in the tie breaker
+        match.pointWonBy("player 2");
+        match.pointWonBy("player 2");
+        assertEquals("6-6, 0-2", match.score());
+
+        match.pointWonBy("player 1");
+        match.pointWonBy("player 1");
+        assertEquals("6-6, 2-2", match.score());
+
+        // Check for normal Deuce functionality
+        match.pointWonBy("player 1");
+        match.pointWonBy("player 2");
+        assertEquals("6-6, Deuce", match.score());
+
+        match.pointWonBy("player 1");
+        match.pointWonBy("player 1");
+
+        assertEquals("6-6, 5-3", match.score());
+
+        match.pointWonBy("player 1");
+        assertEquals("6-6, 6-3", match.score());
+
+        // Test Advantage
+        match.pointWonBy("player 2");
+        match.pointWonBy("player 2");
+        assertEquals("6-6, Advantage player 1", match.score());
+
+        // Test tie break win
+        match.pointWonBy("player 1");
+        assertEquals("7-6", match.score());
     }
 }
